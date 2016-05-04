393586cb1338534afc6709b0a60c978b6ffcb55b
leopard318 <danhna318@gmail.com>
2016-05-04 17:32:39 +0700

    Added files via upload

---
+++ src/shape/SmallSquare.java
@@ -0,0 +1,16 @@
+package shape;
+
+public class SmallSquare extends Square {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SmallSquare()
+	{
+		this.setHeight(25);
+		this.setWidth(25);
+	}
+	
+}
